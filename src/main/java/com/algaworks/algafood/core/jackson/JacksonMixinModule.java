a163d4445c83129327a083d7fc360f17f233ad25
Tadeu Filho <jose.vanderlei@fcamara.com.br>
2022-06-24 23:11:03 -0300

    Adicionando customização mixing para classes Cidade e Cozinha

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.jackson;
 
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.model.mixin.CidadeMixin;
+import com.algaworks.algafood.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.model.mixin.RestauranteMixin;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
@@ -9,6 +13,9 @@ import org.springframework.stereotype.Component;
 public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
+
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
     }
 }
